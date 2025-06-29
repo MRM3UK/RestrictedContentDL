6fec22d82c180d3eeb4b924c862eea4651887b36
MRM3UK <mynewp570@gmail.com>
2025-06-29 13:32:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,17 +10,16 @@ try:
 except:
     pass
 
-    if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
-        print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
-        exit(1)
+if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
+    print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
+    exit(1)
 
-    if (
-        not getenv("SESSION_STRING")
-        or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
-    ):
-        print("Error: SESSION_STRING must be set with a valid string")
-        exit(1)
-
+if (
+    not getenv("SESSION_STRING")
+    or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
+):
+    print("Error: SESSION_STRING must be set with a valid string")
+    exit(1)
 
 # Pyrogram setup
 class PyroConf(object):
@@ -29,3 +28,4 @@ class PyroConf(object):
     BOT_TOKEN = getenv("BOT_TOKEN")
     SESSION_STRING = getenv("SESSION_STRING")
     BOT_START_TIME = time()
+    LOG_CHANNEL_ID = int(getenv("LOG_CHANNEL_ID", "0"))  # ✅ Add this line
